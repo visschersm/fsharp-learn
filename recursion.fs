5e2740dd6fc858c92975e721791dbcb13f4a86cd
Mark Visschers <visschers.m@gmail.com>
2021-06-30 22:59:45 +0200

    some extra practice

--- recursion.fs
+++ recursion.fs
@@ -33,3 +33,29 @@ times2 << minus1 <| 9
 let mul (x: int) (y: int) = x * y
 let add (x: int) (y: int) = x + y
 let pythagoras (x: int) (y: int) = add (mul x x) (mul y y)
+
+let add x y = x + y
+let mul x y = x * y
+
+let foo = add 1 2
+    |> add 3
+
+let rec foo n = 
+    if n <= 0 
+    then 0
+    else n + foo (n - 1)
+
+let rec factorial n = 
+    if n <= 0 
+    then 1
+    else n * factorial (n - 1)
+
+// 1 1 2 3 5 8
+let rec fibonacci n =
+    if n <= 0 then 0
+    elif n = 1 then 1
+    else fibonacci (n - 1) + fibonacci (n - 2)
+
+let fibonacciList n =
+    for i in 0 .. n do
+        printfn "%i" (fibonacci i)
